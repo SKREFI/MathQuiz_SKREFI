77e8c6f38302d5fefdd98e0a1766175d2a92ec78
SKREFI <srazvan43@yahoo.com>
2018-02-27 23:40:14 +0200

    Udacity project

--- app/src/main/java/com/skrefi/mathquiz/MainActivity.java
+++ app/src/main/java/com/skrefi/mathquiz/MainActivity.java
@@ -10,23 +10,10 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    //Wanted to check if the text is the same with the corect one from here but found a better method.
-  /*
-    String answers[][] = {
-            {"sqrt {2}/2","1/2","sqrt {3}/4","1"},
-            {"sin a * cos b + sin b * cos a","sin a + sin b","cos a * cos b + sin b * sin a","1/2"},
-            {"sin(a+b)","ctg a / tg b","2 sin((a+b)/2) * cos ((a-b)/2)","sin a + cos b / cos b + sin a"},
-            {"sqrt{3}/2","0","1","2/3"},
-            {"cos a - cos b","cos^2 a - sin^2 b","sin b / sin a","cos a * cos b + sin a * sin b"},
-            {"sin x + cos x","0","1","sin x * cos x * cos x * sin x"},
-    };
-    */
     RadioGroup[] rg = new RadioGroup[6];
     RadioButton rb;
-
+    Boolean[] rbAnswered = new Boolean[10];
     String name;
-    //public int correctAnswers[]={1,1,3,2,4,3};  //Maybe I use it and anyways it's good for me to see the right answers
-    public boolean s7 = false; // The boolean for the check box at the 7th question
     public int score = 0; //keep track of the score
 
     @Override
@@ -45,75 +32,111 @@ public class MainActivity extends AppCompatActivity {
         name = nameET.getText().toString();
     }
 
-    public void question7clicked(View view) {
-        if (((CheckBox) view).isChecked()) {
-            ((CheckBox) view).setText("So you say it's true");
-            s7 = true;
+    public void cb72(View view){
+        if(((CheckBox) view).isChecked() && !rbAnswered[6]) {
             score++;
-        } else {
-            ((CheckBox) view).setText("So you say it's false");
-            s7 = false;
+            rbAnswered[6]=true;
+        }
+        if(!((CheckBox) view).isChecked() && rbAnswered[6]) {
             score--;
+            rbAnswered[6]=false;
+        }
+    }
+
+    public void cb73(View view){
+        if(((CheckBox) view).isChecked() && !rbAnswered[7]){
+            score++;
+            rbAnswered[7]=true;
+        }
+        if(!((CheckBox) view).isChecked() && rbAnswered[7]) {
+            score--;
+            rbAnswered[7]=false;
+        }
+    }
+
+    public void cb74(View view){
+        if(((CheckBox) view).isChecked() && !rbAnswered[8]){
+            score++;
+            rbAnswered[8]=true;
+        }
+        if(!((CheckBox) view).isChecked() && rbAnswered[8]) {
+            score--;
+            rbAnswered[8]=false;
         }
     }
 
     public void rb1Click(View v) {
         int rbi = rg[0].getCheckedRadioButtonId();
         rb = findViewById(R.id.rb11);
-        if (rbi == rb.getId()) {
-            score++;  //the answer is right if the checked radio id is the same with the one I know it's correct
-        } else {
+        if (rbi == rb.getId() && !rbAnswered[0]) {
+                score++;
+                rbAnswered[0]=true;
+        }
+        if(rbi != rb.getId() && rbAnswered[0]){
             score--;
+            rbAnswered[0]=false;
         }
     }
 
     public void rb2Click(View v) {
         int rbi = rg[1].getCheckedRadioButtonId();
         rb = findViewById(R.id.rb21);
-        if (rbi == rb.getId()) {
+        if (rbi == rb.getId() && !rbAnswered[1]) {
             score++;
-        } else {
+            rbAnswered[1]=true;
+        }
+        if(rbi != rb.getId() && rbAnswered[1]){
             score--;
+            rbAnswered[1]=false;
         }
     }
 
     public void rb3Click(View v) {
-        if (rg[2].getCheckedRadioButtonId() == R.id.rb33) {
+        if (rg[2].getCheckedRadioButtonId() == R.id.rb33 && !rbAnswered[2]) {
             score++;
-        } else {
+            rbAnswered[2]=true;
+        }
+        if (rg[2].getCheckedRadioButtonId() != R.id.rb33 && rbAnswered[2]) {
             score--;
+            rbAnswered[2]=false;
         }
     }
 
     public void rb4Click(View v) {
-        if (rg[3].getCheckedRadioButtonId() == R.id.rb42) {
+        if (rg[3].getCheckedRadioButtonId() == R.id.rb42 && !rbAnswered[3]) {
             score++;
-        } else {
+            rbAnswered[3]=true;
+        }
+        if (rg[2].getCheckedRadioButtonId() != R.id.rb42 && rbAnswered[3]) {
             score--;
+            rbAnswered[3]=false;
         }
     }
 
     public void rb5Click(View v) {
-        if (rg[4].getCheckedRadioButtonId() == R.id.rb54) {
+        if (rg[4].getCheckedRadioButtonId() == R.id.rb54 && !rbAnswered[4]) {
             score++;
-        } else {
+            rbAnswered[4]=true;
+        }
+        if (rg[4].getCheckedRadioButtonId() != R.id.rb54 && rbAnswered[4]) {
             score--;
+            rbAnswered[4]=false;
         }
     }
 
     public void rb6Click(View v) {
-        if (rg[5].getCheckedRadioButtonId() == R.id.rb63) {
+        if (rg[5].getCheckedRadioButtonId() == R.id.rb63 && !rbAnswered[5]) {
             score++;
-        } else {
+            rbAnswered[5]=true;
+        }
+        if (rg[5].getCheckedRadioButtonId() != R.id.rb63 && rbAnswered[5]) {
             score--;
+            rbAnswered[5]=false;
         }
     }
 
     public void submit(View v) {
-        if (score < 0) {
-            score = 0;
-        }
-        Toast.makeText(getBaseContext(), "Congrats" + name + ": " + score + " out of 7!", Toast.LENGTH_LONG).show();
+        Toast.makeText(getBaseContext(), "Congrats" + name + ": " + score + " out of 10!", Toast.LENGTH_LONG).show();
     }
 
 }
